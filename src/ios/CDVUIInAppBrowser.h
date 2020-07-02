855207a6cab0d0e35db5b0c48813c7860070a087
Augustine Cheong <ts-augustine.cheong@rakuten.com>
2020-07-02 13:36:59 +0900

    Fixed the height of the webview

--- src/ios/CDVUIInAppBrowser.h
+++ src/ios/CDVUIInAppBrowser.h
@@ -82,6 +82,9 @@
 @property (nonatomic, weak) CDVUIInAppBrowser* navigationDelegate;
 @property (nonatomic) NSURL* currentURL;
 
+@property (nonatomic, strong) UIToolbar* statusBarBackground;
+@property (nonatomic) BOOL shouldUpdateToolbarHeight; //default YES, will be set to NO after viewWillAppear
+
 - (void)close;
 - (void)navigateTo:(NSURL*)url;
 - (void)showLocationBar:(BOOL)show;
